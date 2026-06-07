ab5f8bea04768f253cf1b849f75f39eaa72e31f4
Shubhi Gupta <shubhigupta0446@gmail.com>
2026-06-07 11:28:40 +0530

    Add missing React frontend components (App.jsx and main.jsx)

--- event-frontend/src/App.jsx
+++ event-frontend/src/App.jsx
@@ -1,24 +1,14 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Login from "./components/Login";
-import Events from "./components/Events";
-import MyRegistrations from "./components/MyRegistrations";
-import ProtectedRoute from "./components/ProtectedRoute";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
 
-const App = () => {
+function App() {
   return (
-    <>
-      <Navbar />
+    <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/events" element={<ProtectedRoute><Events /></ProtectedRoute>} />
-        <Route path="/registrations" element={<ProtectedRoute><MyRegistrations /></ProtectedRoute>} />
-        <Route path="*" element={<Login />} />  {/* catches all unmatched URLs */}
+        <Route path="/" element={<div className="container"><h1>Event Ticket Generator</h1></div>} />
       </Routes>
-    </>
-  );
-};
+    </Router>
+  )
+}
 
-export default App;
+export default App
